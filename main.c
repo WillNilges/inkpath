2feab4a811f6955f149d9a9d5eb8fa74d5e98ab8
Will Nilges <wdn5796@rit.edu>
2021-02-27 15:39:10 -0500

    Refactoring part 1

--- main.c
+++ main.c
@@ -16,6 +16,8 @@
 void invoke_autotrace(char* input_file, char* output_file, int color_count, char* background);
 char *regexp (char* string, regex_t* rgT, int* begin, int* end);
 int* get_colors(char* input_file, int max_colors);
+void dump_to_xoj();
+void get_points();
 
 void invoke_autotrace(char* input_file, char* output_file, int color_count, char* background)
 {
@@ -59,13 +61,11 @@ void invoke_autotrace(char* input_file, char* output_file, int color_count, char
 }
 
 // https://cboard.cprogramming.com/c-programming/117525-regex-h-extracting-matches.html
-char *regexp (char* string, regex_t* rgT, int* begin, int* end)
+char* regexp(char* string, regex_t* rgT, int* begin, int* end)
 { 
     int i, w=0, len;                  
     char *word = NULL;
-    // regex_t rgT;
     regmatch_t match;
-    // regcomp(&rgT,patrn,REG_EXTENDED);
     if ((regexec(rgT,string,1,&match,0)) == 0) {
             *begin = (int)match.rm_so;
             *end = (int)match.rm_eo;
@@ -167,16 +167,17 @@ void dump_to_xoj() {
     if (!fp)
     {
         fprintf(stderr, "Error opening file '%s'\n", input_file);
-        // return NULL;
+        return;
     }
 
     /* Open output file for writing */
     FILE* outptr;
     outptr = fopen(output_file, "w");
-    // fprintf(outptr, "<stroke tool=\"pen\" ts=\"0ll\" fn=\"\" color=\"#3333ccff\" width=\"1.41000000\">");
 
     // File head
-    fprintf(outptr, "<?xml version=\"1.0\" standalone=\"no\"?>\n<xournal version=\"0.4.8.2016\">\n<title>Xournal document - see http://math.mit.edu/~auroux/software/xournal/</title>\n<page width=\"612.00\" height=\"792.00\">\n<background type=\"solid\" color=\"white\" style=\"lined\" />\n<layer>\n<stroke tool=\"pen\" color=\"black\" width=\"1.41\">\n");
+    fprintf(outptr, "<?xml version=\"1.0\" standalone=\"no\"?>\n<xournal version=\"0.4.8.2016\">\n<title>Xournal document - see http://math.mit.edu/~auroux/software/xournal/</title>\n<page width=\"612.00\" height=\"792.00\">\n<background type=\"solid\" color=\"white\" style=\"lined\" />\n<layer>\n");
+
+    char* start_stroke = "<stroke tool=\"pen\" color=\"black\" width=\"1.41\">\n";
 
     /* Get the first line of the file. */
     line_size = getline(&line_buf, &line_buf_size, fp);
@@ -193,7 +194,6 @@ void dump_to_xoj() {
     /* Loop through until we are done with the file. */
     while (line_size >= 0)
     {
-
         /*
             This code will take a line buffer in that looks like this:
             1675.2338981628418,3409.3245763778687
@@ -213,6 +213,7 @@ void dump_to_xoj() {
         */
 
         xy_split = strchr(line_buf, ',');
+
         if (xy_split)
         {
             splitloc = xy_split-line_buf;
@@ -223,7 +224,6 @@ void dump_to_xoj() {
             snprintf(x_coord, 50, "%.*s", splitloc, line_buf);
             snprintf(y_coord, 50, "%.*s", y_length, y_substring);
 
-            // line_buf[splitloc] = ' '; // This is fucked. Don't do this.
             printf("%s | %s\n", x_coord, y_coord);
 
             x_double = atof(x_coord);
@@ -239,7 +239,6 @@ void dump_to_xoj() {
         
         /* Get the next line */
         line_size = getline(&line_buf, &line_buf_size, fp);
-        // break;
     }
 
     free(x_coord);
@@ -259,9 +258,6 @@ void dump_to_xoj() {
 }
 
 void get_points() {
-
-    int i;
-
     /* Open output file for writing */
     char* output_file = "pointlist.txt";
     FILE* outptr;
@@ -281,11 +277,9 @@ void get_points() {
     {
 		for (path = shape->paths; path != NULL; path = path->next)
         {
-			// drawPath(path->pts, path->npts, path->closed, px * 1.5f);
-			// drawControlPts(path->pts, path->npts);
             float* pts = path->pts;
             int npts = path->npts;
-            for (i = 0; i < npts-1; i += 3)
+            for (int i = 0; i < npts-1; i += 3)
             {
                 float* p = &pts[i*2];
                 // glVertex2f(p[6],p[7]);
