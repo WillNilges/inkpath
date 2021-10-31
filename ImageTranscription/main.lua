6ea909a1bdb037334901684bc1d2a4a2ce77faf1
Will Nilges <wdn5796@rit.edu>
2021-10-31 10:13:05 -0400

    Remove extras from lua plugin

--- ImageTranscription/main.lua
+++ ImageTranscription/main.lua
@@ -1,23 +1,14 @@
 -- This is an example Xournal++ Plugin - copy this to get started
 
--- var_dump = require "var_dump"
-
 -- Register all Toolbar actions and intialize all UI stuff
 function initUi()
-  print("Hello from ImageTranscription: Plugin initUi called\n");
-
   ref = app.registerUi({["menu"] = "Transcribe Image", ["callback"] = "drawStroke", ["accelerator"] = "<Control><Shift>t"});
-  -- print("Menu reference:");
-  -- var_dump(ref);
-
   print("ImageTranscription registered\n");
 end
 
 -- Callback if the menu item is executed
 function drawStroke()
-  -- app.msgbox("Processing image; Please wait.", {})
   inkpath = require 'inkpath'
-  local inspect = require 'inspect'
   path = app.getFilePath()
   strokes = inkpath.transcribe_image(path)
   print("Strokes retrieved.")
