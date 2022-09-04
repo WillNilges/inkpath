d788e7470237710de77660f857e6b45f7302d83c
Will Nilges <wdn5796@rit.edu>
2022-09-04 17:55:34 -0400

    Add python

---
+++ src/cv/otsu.py
@@ -0,0 +1,32 @@
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+img = cv.imread('../../inkpath_sample_images/roland.jpg',0)
+# global thresholding
+ret1,th1 = cv.threshold(img,127,255,cv.THRESH_BINARY)
+# Otsu's thresholding
+ret2,th2 = cv.threshold(img,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
+# Otsu's thresholding after Gaussian filtering
+blur = cv.GaussianBlur(img,(5,5),0)
+ret3,th3 = cv.threshold(blur,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
+# plot all the images and their histograms
+images = [img, 0, th1,
+          img, 0, th2,
+          blur, 0, th3]
+titles = ['Original Noisy Image','Histogram','Global Thresholding (v=127)',
+          'Original Noisy Image','Histogram',"Otsu's Thresholding",
+          'Gaussian filtered Image','Histogram',"Otsu's Thresholding"]
+
+solo = True
+if solo == True:
+    plt.imshow(images[2*3+2], 'gray')
+    plt.xticks([]), plt.yticks([])
+else:
+    for i in range(3):
+        plt.subplot(3,3,i*3+1),plt.imshow(images[i*3],'gray')
+        plt.title(titles[i*3]), plt.xticks([]), plt.yticks([])
+        plt.subplot(3,3,i*3+2),plt.hist(images[i*3].ravel(),256)
+        plt.title(titles[i*3+1]), plt.xticks([]), plt.yticks([])
+        plt.subplot(3,3,i*3+3),plt.imshow(images[i*3+2],'gray')
+        plt.title(titles[i*3+2]), plt.xticks([]), plt.yticks([])
+plt.show()
