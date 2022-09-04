597b4212047b1536b4b85620c15745030c26f6b9
Will Nilges <wdn5796@rit.edu>
2022-09-03 20:36:04 -0400

    Forward computer -> freedom -> container

--- HACKING/launch-environment.sh
+++ HACKING/launch-environment.sh
@@ -8,17 +8,22 @@ if [ "$(pwd)" == "$HOME" ]; then
     exit 1
 fi
 
-CODE_PATH=/home/$USER/Code/xopp-dev
+CODE_PATH="$HOME"/Code/xopp-dev
 uname=$USER # Just trust me on this one.
+
+# For changing the group
+if [ -n "$1" ]; then
+    uname=$1
+fi
 
 xauth_path=/tmp/xopp-dev-xauth
 
 rm -rf "$xauth_path"
 mkdir -p "$xauth_path"
-cp $HOME/.Xauthority "$xauth_path"
+cp "$HOME"/.Xauthority "$xauth_path"
 chmod g+rwx "$xauth_path"/.Xauthority
 #chmod g+rwx "$CODE_PATH"
-podman unshare chown 1000:$uname "$xauth_path"/.Xauthority
+#podman unshare chown 1000:"$uname" "$xauth_path"/.Xauthority # Chom
 #podman unshare chown 1000:$uname "$CODE_PATH"
 #podman unshare chown 1000:$uname -R "$CODE_PATH"
 #sudo chmod -R +070 "$CODE_PATH"
@@ -26,12 +31,12 @@ podman unshare chown 1000:$uname "$xauth_path"/.Xauthority
 #sudo chown -R :$uname "$xauth_path"/.Xauthority
 
 podman run --name=xopp-dev --rm -it                   \
-    -e DISPLAY=$DISPLAY                               \
+    -e DISPLAY="$DISPLAY"                             \
     --network=host                                    \
     --cap-add=SYS_PTRACE                              \
     --group-add keep-groups                           \
     --annotation io.crun.keep_original_groups=1       \
-    -v "$xauth_path"/.Xauthority:/root/.Xauthority:rw \
+    -v "$xauth_path"/.Xauthority:/root/.Xauthority:Z \
     -v "$CODE_PATH":/xopp-dev:Z                       \
     -v /tmp/.X11-unix:/tmp/.X11-unix xopp-dev
 rm -rf "$xauth_path"
