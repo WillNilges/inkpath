3f12d79fe8bc9f983c27174e40e0feff309e0043
Will Nilges <wdn5796@rit.edu>
2022-09-20 20:06:10 -0400

    rebuild header

---
+++ src/cv/ipcv.h
@@ -0,0 +1,8 @@
+
+
+
+Mat skeletonize(Mat img_inv, std::string output_path);
+Mat otsu(Mat img, std::string output_path);
+Shapes find_shapes(Mat img, std::string output_path);
+void prep_otsu(char* image_path);
+
