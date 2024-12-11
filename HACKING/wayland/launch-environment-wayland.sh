1b15f6865a5df8ded9854f4ea70bd93eb21576f8
Willard Nilges <willardnilges@gmail.com>
2024-12-10 23:48:25 -0500

    Add new wayland dev container

--- HACKING/wayland/launch-environment-wayland.sh
+++ HACKING/wayland/launch-environment-wayland.sh
@@ -2,8 +2,11 @@ user_uid=$UID
 user_gid=$(id -rg)
 user_name=$USER
 
-echo $XDG_RUNTIME_DIR
-echo $WAYLAND_DISPLAY
+CODE_PATH="$HOME"/Code/xopp-dev
+
+cd $CODE_PATH/inkpath/HACKING/
+
+podman build . --tag xopp-dev-wayland --file ./wayland/Dockerfile
 
 podman run \
     --rm \
@@ -11,7 +14,9 @@ podman run \
     -e XDG_RUNTIME_DIR=/tmp \
     -e "WAYLAND_DISPLAY=$WAYLAND_DISPLAY" \
     -v "$XDG_RUNTIME_DIR/$WAYLAND_DISPLAY:/tmp/$WAYLAND_DISPLAY:ro" \
-    -v /run/user/$user_uid/wayland-0:/tmp/wayland-0:ro -it fedora \
+    -v "/run/user/$user_uid/wayland-0:/tmp/wayland-0:ro" \
+    -v "$CODE_PATH":/xopp-dev:Z \
+    -it xopp-dev-wayland
     #bash -euo pipefail -c "
     #    dnf install -y \
     #        coreutils bash bash-completion less nano sudo tree util-linux gedit
