092bb1aacfe7b9dd75bc15593ff55fe0365e65ae
Willard Nilges <willardnilges@gmail.com>
2024-12-25 20:39:26 -0500

    Add locals to shut up IDE

--- ImageTranscription/main.lua
+++ ImageTranscription/main.lua
@@ -7,23 +7,23 @@ end
 -- Callback if the menu item is executed
 function drawStroke()
     print("Inkpath Activated. Transcribing image....")
-    inkpath = assert(package.loadlib("/usr/share/xournalpp/plugins/ImageTranscription/ipcvobj.so", "luaopen_ipcvobj"))
+    local inkpath = assert(package.loadlib("/usr/share/xournalpp/plugins/ImageTranscription/ipcvobj.so", "luaopen_ipcvobj"))
     inkpath()
-    path = app.getFilePath({'*.jpg', '*.png', '*.bmp'}) -- The current version of Autotrace I'm using only supports PNGs.
+    local path = app.getFilePath({'*.jpg', '*.png', '*.bmp'}) -- The current version of Autotrace I'm using only supports PNGs.
     --image_scale = app.msgbox("Select tracing scale", {[1] = "Small", [2] = "Medium", [3] = "Large"}) -- TODO: implement this again.
-    image_scale = 1
-    scaling_factor = 10.0 -- how much do you want to divide your content by? must be float!
+    local image_scale = 1
+    local scaling_factor = 10.0 -- how much do you want to divide your content by? must be float!
     local obj = IPCVObj(path, 1)
     print("Strokes retrieved.")
-    contourCt = obj:getLength()
+    local contourCt = obj:getLength()
     print("Got ", contourCt, " strokes.")
     -- TODO: This could be much, MUCH faster.
     for i = 0,contourCt-1,1 do
-        pointCt = obj:getContourLength(i)
+        local pointCt = obj:getContourLength(i)
         -- We have no use for strokes that are less than two points---we can't do
         -- anything with them.
         if pointCt >= 3 then
-            x_points, y_points = obj:getContour(i, scaling_factor)
+            local x_points, y_points = obj:getContour(i, scaling_factor)
             app.addStrokes({
                 ["strokes"] = {
                     {
