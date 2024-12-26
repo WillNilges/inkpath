6dc732c5641e375ace8a6708e126c8c7e2e6c1a7
Willard Nilges <wdn5796@rit.edu>
2024-12-25 21:13:39 -0500

    Add a few optimizations to Lua script (#39)
    * Add a few optimizations to Lua script
    
    * Add locals to shut up IDE

--- ImageTranscription/main.lua
+++ ImageTranscription/main.lua
@@ -7,29 +7,34 @@ end
 -- Callback if the menu item is executed
 function drawStroke()
     print("Inkpath Activated. Transcribing image....")
-    inkpath = assert(package.loadlib("/usr/share/xournalpp/plugins/ImageTranscription/ipcvobj.so", "luaopen_ipcvobj"))
+    local inkpath = assert(package.loadlib("/usr/share/xournalpp/plugins/ImageTranscription/ipcvobj.so", "luaopen_ipcvobj"))
     inkpath()
-    path = app.getFilePath({'*.jpg', '*.png', '*.bmp'}) -- The current version of Autotrace I'm using only supports PNGs.
+    local path = app.getFilePath({'*.jpg', '*.png', '*.bmp'}) -- The current version of Autotrace I'm using only supports PNGs.
     --image_scale = app.msgbox("Select tracing scale", {[1] = "Small", [2] = "Medium", [3] = "Large"}) -- TODO: implement this again.
-    image_scale = 1
-    scaling_factor = 10.0 -- THIS IS A NEW THING! HOW MUCH DO YOU WANT TO DIVIDE YOUR SHIT BY!? MUST BE FLOAT!
+    local image_scale = 1
+    local scaling_factor = 10.0 -- how much do you want to divide your content by? must be float!
     local obj = IPCVObj(path, 1)
     print("Strokes retrieved.")
-    contourCt = obj:getLength()
+    local contourCt = obj:getLength()
     print("Got ", contourCt, " strokes.")
     -- TODO: This could be much, MUCH faster.
     for i = 0,contourCt-1,1 do
-        pointCt = obj:getContourLength(i)
-        x_points, y_points = obj:getContour(i, scaling_factor)
-        app.addStrokes({
-            ["strokes"] = {
-                {
-                    ["x"] = x_points,
-                    ["y"] = y_points,
+        local pointCt = obj:getContourLength(i)
+        -- We have no use for strokes that are less than two points---we can't do
+        -- anything with them.
+        if pointCt >= 3 then
+            local x_points, y_points = obj:getContour(i, scaling_factor)
+            app.addStrokes({
+                ["strokes"] = {
+                    {
+                        ["x"] = x_points,
+                        ["y"] = y_points,
+                        ["tool"] = "pen", -- Default to pen to silence warnings. TODO: Delete in the future.
+                    },
                 },
-            },
-            ["allowUndoRedoAction"] = "grouped",
-        })
+                ["allowUndoRedoAction"] = "grouped",
+            })
+        end
     end
     app.refreshPage() -- Refreshes the page after inserting the strokes.
     print("Image Transcription Complete. Exiting Inkpath.")
